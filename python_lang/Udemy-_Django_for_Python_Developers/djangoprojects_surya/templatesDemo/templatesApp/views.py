9c9727116baa2d8d22fb38127c6db1687bce006c
jamwalshah <suryadevsinghjamwal01@gmail.com>
2024-07-04 08:01:13 +0530

    updated 05. Templates files

--- python_lang/Udemy-_Django_for_Python_Developers/djangoprojects_surya/templatesDemo/templatesApp/views.py
+++ python_lang/Udemy-_Django_for_Python_Developers/djangoprojects_surya/templatesDemo/templatesApp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def renderTemplate(request):
+    return render(request=request, template_name='templatesApp/firstTemplate.html')
--- python_lang/Udemy-_Django_for_Python_Developers/djangoprojects_surya/templatesDemo/templatesDemo/urls.py
+++ python_lang/Udemy-_Django_for_Python_Developers/djangoprojects_surya/templatesDemo/templatesDemo/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from templatesApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(route='firstTemplate/', view=views.renderTemplate),
 ]
