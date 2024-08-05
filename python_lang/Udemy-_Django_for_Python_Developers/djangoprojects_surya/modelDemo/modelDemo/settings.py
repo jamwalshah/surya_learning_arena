96e50a658b40fde66c86f571dee77ca8857cb147
jamwalshah <suryadevsinghjamwal01@gmail.com>
2024-08-06 00:00:46 +0530

    updated 06. Models files

--- python_lang/Udemy-_Django_for_Python_Developers/djangoprojects_surya/modelDemo/modelDemo/settings.py
+++ python_lang/Udemy-_Django_for_Python_Developers/djangoprojects_surya/modelDemo/modelDemo/settings.py
@@ -83,6 +83,12 @@ DATABASES = {
         'PASSWORD': 'mysql',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
