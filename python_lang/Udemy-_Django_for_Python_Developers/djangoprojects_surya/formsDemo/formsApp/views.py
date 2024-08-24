189ed36c315a45caf2f2e8950a0707f384bd5bd4
jamwalshah <suryadevsinghjamwal01@gmail.com>
2024-08-24 20:34:51 +0530

    updated 08. Forms files

--- python_lang/Udemy-_Django_for_Python_Developers/djangoprojects_surya/formsDemo/formsApp/views.py
+++ python_lang/Udemy-_Django_for_Python_Developers/djangoprojects_surya/formsDemo/formsApp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from . import forms
 
 # Create your views here.
+def userRegistrationView(request):
+    form = forms.UserRegistrationForm()
+    return render(request=request, template_name='formsDemo/userRegistration.html', context={'form':form})
