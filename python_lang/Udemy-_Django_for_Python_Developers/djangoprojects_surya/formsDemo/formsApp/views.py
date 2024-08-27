533e7eea85b933f7d3cb0c0f02c6df8e3833172d
jamwalshah <suryadevsinghjamwal01@gmail.com>
2024-08-27 21:48:43 +0530

    Updated 08. Forms files

--- python_lang/Udemy-_Django_for_Python_Developers/djangoprojects_surya/formsDemo/formsApp/views.py
+++ python_lang/Udemy-_Django_for_Python_Developers/djangoprojects_surya/formsDemo/formsApp/views.py
@@ -4,4 +4,13 @@ from . import forms
 # Create your views here.
 def userRegistrationView(request):
     form = forms.UserRegistrationForm()
+    if request.method == 'POST':
+        form = forms.UserRegistrationForm(request.POST) # form object will have all the data that user submits
+        if form.is_valid():
+            print("Form is valid")
+            print("First Name", form.cleaned_data['firstName'])
+            print("Last Name", form.cleaned_data['lastName'])
+            print("Email", form.cleaned_data['email'])
+            # print("form:", form) # debug
+            # print("form.cleaned_data:", form.cleaned_data) # debug
     return render(request=request, template_name='formsDemo/userRegistration.html', context={'form':form})
