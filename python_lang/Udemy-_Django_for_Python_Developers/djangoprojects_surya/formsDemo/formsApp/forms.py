6aa0bfde71225a40e4fa38da9f098086fa03f797
jamwalshah <suryadevsinghjamwal01@gmail.com>
2024-09-09 05:33:22 +0530

    Updated 08. Forms files

--- python_lang/Udemy-_Django_for_Python_Developers/djangoprojects_surya/formsDemo/formsApp/forms.py
+++ python_lang/Udemy-_Django_for_Python_Developers/djangoprojects_surya/formsDemo/formsApp/forms.py
@@ -1,9 +1,33 @@
+from typing import Any
 from django import forms
 
 class UserRegistrationForm(forms.Form):
     GENDER = [('male', 'MALE'), ('female', 'FEMALE')]
-    firstName = forms.CharField()
+    firstName = forms.CharField(required=False)
     lastName = forms.CharField()
     email = forms.CharField()
     gender = forms.CharField(widget=forms.Select(choices = GENDER))
     password = forms.CharField(widget=forms.PasswordInput)
+    ssn = forms.IntegerField()
+    
+    def clean(self):
+        user_cleaned_data = super().clean()
+        inputFirstName = user_cleaned_data['firstName']
+        if len(inputFirstName) > 20:
+            raise forms.ValidationError(message='The Max length of firstName is 20 characters')
+        inputEmail = user_cleaned_data["email"]
+        if inputEmail.find('@') == -1:
+            raise forms.ValidationError(message='The email should contain @')
+        
+    '''
+    def clean_firstName(self):
+        inputFirstName = self.cleaned_data["firstName"]
+        if len(inputFirstName) > 20:
+            raise forms.ValidationError(message='The Max length of firstName is 20 characters')
+        return inputFirstName
+    def clean_email(self):
+        inputEmail = self.cleaned_data["email"]
+        if inputEmail.find('@') == -1:
+            raise forms.ValidationError(message='The email should contain @')
+        return inputEmail
+    '''
