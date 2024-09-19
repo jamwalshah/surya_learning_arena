1a69e49b581918c9ab1b20a81667c877b32156eb
jamwalshah <suryadevsinghjamwal01@gmail.com>
2024-09-19 23:37:30 +0530

    updated 10. CRUD using Function Based Views md and its project files

--- python_lang/Udemy-_Django_for_Python_Developers/djangoprojects_surya/fbvCRUD/fbvApp/views.py
+++ python_lang/Udemy-_Django_for_Python_Developers/djangoprojects_surya/fbvCRUD/fbvApp/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from fbvApp.models import Student
+from fbvApp.forms import StudentForm
 
 # Create your views here.
+def getStudents(request):
+    students = Student.objects.all()
+    return render(request=request, template_name='fbvApp/index.html', context={'students':students})
+
+def createStudent(request):
+    form = StudentForm()
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect(to='/')
+    return render(request=request, template_name='fbvApp/create.html', context={'form':form})
