f76a4c76e33432277665c5eb8af4ff3da696e8a6
jamwalshah <suryadevsinghjamwal01@gmail.com>
2024-09-23 23:50:54 +0530

    updated 10. CRUD using Function Based Views md and its project files

--- python_lang/Udemy-_Django_for_Python_Developers/djangoprojects_surya/fbvCRUD/fbvApp/views.py
+++ python_lang/Udemy-_Django_for_Python_Developers/djangoprojects_surya/fbvCRUD/fbvApp/views.py
@@ -20,3 +20,12 @@ def deleteStudent(request, id):
     student = Student.objects.get(id=id)
     student.delete()
     return redirect(to='/')
+
+def updateStudent(request, id):
+    student = Student.objects.get(id=id)
+    if request.method == 'POST':
+        form = StudentForm(data=request.post, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect(to='/')
+    return render(request=request, template_name='fbvApp/update.html', context={'student':student})
